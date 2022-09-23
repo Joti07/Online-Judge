a590e70097435bf9d322695c9882296fce30c906
Tanjila Sultana Joti <44638932+Joti07@users.noreply.github.com>
2022-09-23 16:47:37 +0600

    Add files via upload

---
+++ LEETCODE/69. Sqrt(x).cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define mx 10005
+using namespace std;
+typedef double ll;
+int mySqrt(int x)
+{
+    return(sqrt(x));
+
+}
+int main()
+{
+   int n;
+   cin>>n;
+    cout<<mySqrt(n)<<endl;
+}
+//     cout<<fixed<<setprecision(2)<<(double)1/2<<endl;
+//     cin.ignore();
+//     getchar();
+//     cout<<"Case "<<tc++<<": ";
